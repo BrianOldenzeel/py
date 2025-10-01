4b71b7be9a21b07731b35cc2b3ac26d520cecc67
BrianOldenzeel <blianoldenzeel@gmail.com>
2025-10-01 19:52:25 +0200

    week 7 basis

--- opdrachtweek5.py
+++ opdrachtweek5.py
@@ -2,35 +2,32 @@
 # Voorbeeldprogramma voor een lus voor gebruikersinteractie
 #  (een variant van de versie van het college)
 #
+import math
 
 
 def menu():
     """A function that simply prints the menu"""
     print()
-    print("(0) Doorgaan!")
-    print("(1) Nieuwe lijst invoeren")
-    print("(2) Volgende element voorspellen")
-    print("(9) Stoppen! (einde)")
-    print()
+    print("(0) Voer een nieuwe lijst in")
+    print("(1) Druk de huidige lijst af")
+    print("(2) Bepaal de gemiddelde prijs")
+    print("(3) Bepaal de standaardafwijking")
+    print("(4) Bepaal het minimum en de bijbehorende dag")
+    print("(5) Bepaal het maximum en de bijbehorende dag")
+    print("(6) Je TR-investeringsplan")
+    print("(9) Stoppen")
+
 
 
 def predict(L):
-    """Predict ignores its argument and returns
-       what the next element should have been.
-    """
-    return 42
+    min = find_min_loc(L)
 
+    profit = 0
 
-def find_min(L):
-    """find min uses a loop to return the minimum of L.
-       Argument L: a nonempty list of numbers.
-       Return value: the smallest value in L.
-    """
-    result = L[0]
-    for x in L:
-        if x < result:
-            result = x
-    return result
+    for i, item in enumerate(L[min[1]:]):
+        if(item > min[0]):
+            profit = item
+    return profit - min[0]
 
 
 def find_min_loc(L):
@@ -49,6 +46,33 @@ def find_min_loc(L):
 
     return min_val, min_loc
 
+def find_max_loc(L):
+    max_val = L[0]
+    max_loc = 0
+
+    for i in list(range(len(L))):
+        if L[i] > max_val:  # een kleinere gevonden!
+            max_val = L[i]
+            max_loc = i
+
+    return max_val, max_loc
+
+
+def returnAvg(l):
+    total = 0
+    for i in l:
+        total += i
+    
+    return total / len(l)
+
+def calcDif(l):
+    avg = returnAvg(l)
+    squared_diffs = [(x - avg) ** 2 for x in l]
+    variance = sum(squared_diffs) / len(l)
+
+    # Standard deviation
+    std_dev = math.sqrt(variance)
+    return std_dev
 
 def main():
     """The main user-interaction loop"""
@@ -75,10 +99,7 @@ def main():
         if choice == 9:    # We willen stoppen
             break          # De hele while-lus afbreken
 
-        elif choice == 0:  # We willen doorgaan...
-            continue       # Terug naar het begin van de while-lus
-
-        elif choice == 1:  # We willen een nieuwe lijst invoeren
+        elif choice == 0:  # We willen een nieuwe lijst invoeren
             new_L = input("Voer een nieuwe lijst in: ")    # _iets_ invoeren
 
             #
@@ -93,25 +114,35 @@ def main():
             except:
                 print("Ik begreep de invoer niet. L wordt niet aangepast.")
 
-        elif choice == 2:   # Het volgende element voorspellen en toevoegen
-            n = predict(L)  # Het volgende element uit de functie predict halen
-            print("Het volgende element is", n)
-            print("Het wordt toegevoegd aan je lijst...")
-            L = L + [n]     # ...en toevoegen aan de lijst
+        elif choice == 1:  # We willen doorgaan...
+            continue       # Terug naar het begin van de while-lus
+
+            # laat AVG van de lijst zien
+        elif choice == 2:
+            print(returnAvg(L))
+
 
         elif choice == 3:  # Geheime menu-optoe!
-            pass       # Dit is het "nop"- (niets-doen) statement in Python
+            print(calcDif(L))
 
-        elif choice == 4:  # Geheime menu-optie (iets interessanter...)
-            m = find_min(L)
-            print("De kleinste waarde van L is", m)
-
-        elif choice == 5:  # Nog een geheime menu-optie (nog interessanter...)
-            min_val, min_loc = find_min_loc(l)
+        elif choice == 4:  # Nog een geheime menu-optie (nog interessanter...)
+            min_val, min_loc = find_min_loc(L)
             print("De kleinste waarde van L is", min_val, "op dag #", min_loc)
+
+        elif choice == 5:  # Geheime menu-optie (iets interessanter...)
+            max_val, max_loc = find_max_loc(L)
+            print("De grootste waarde van L is", max_val, "op dag #", max_loc)
+
+
+        elif choice == 6:   # Het volgende element voorspellen en toevoegen
+            n = predict(L)  # Het volgende element uit de functie predict halen
+            print("Hoogste winst is: ", n)
+
 
         else:
             print(choice, " ?      Dat staat niet op het menu!")
 
     print()
     print("Tot gisteren!")
+
+main()
