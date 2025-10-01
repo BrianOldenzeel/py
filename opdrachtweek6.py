51897de6ef0c3f542f5d14271d795cd1ff918831
BrianOldenzeel <blianoldenzeel@gmail.com>
2025-10-01 21:29:34 +0200

    .

--- opdrachtweek6.py
+++ opdrachtweek6.py
@@ -1,2 +1,10 @@
-def num_to_base(n, b):
-    
+def num_to_binary(n):
+    number = n
+    binary = ""
+    while number > 0:
+        remain = number % 2
+        number = number // 2  
+        binary = str(remain) + binary
+    return binary
+
+print(num_to_binary(18))
