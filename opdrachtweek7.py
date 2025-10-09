3a00baa5437ec881d4b4f3879f49f63cccbb6d4e
BrianOldenzeel <blianoldenzeel@gmail.com>
2025-10-09 10:55:59 +0200

    week 7 opdracht 1

--- opdrachtweek7.py
+++ opdrachtweek7.py
@@ -1,9 +1,16 @@
 def next(term):
+    term_string = str(term)
     final = ""
-    temp = 0
+    temp = 1
 
-    for index, t in enumerate(str(term)):
-        print("")
+    for index in range(1, len(term_string)):
+        if term_string[index] == term_string[index - 1]:
+            temp += 1
+        else:
+            final += str(temp) + term_string[index - 1]
+            count = 1
+        
+    final += str(temp) + term_string[-1]
 
     return final
 
