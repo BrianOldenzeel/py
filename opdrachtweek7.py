0147a9711b7b3f2d9198048e3bb85a096e958df1
BrianOldenzeel <blianoldenzeel@gmail.com>
2025-10-15 13:52:16 +0200

    week 7 done

--- opdrachtweek7.py
+++ opdrachtweek7.py
@@ -8,10 +8,27 @@ def next(term):
             temp += 1
         else:
             final += str(temp) + term_string[index - 1]
-            count = 1
+            temp = 1
         
     final += str(temp) + term_string[-1]
 
-    return final
+    return int(final)
 
 print(next(21))
+print(next(312211))
+assert next(21) == 1211
+assert next(2222) == 42  # merk op dat dit getal niet voorkomt in de echte rij
+assert next(312211) == 13112221
+
+
+
+def read_it(n):
+    number = str(1)
+    final = ""
+    temp = 1
+
+    for i in range(n):
+        number = next(number)
+        print(number)
+
+read_it(6)
